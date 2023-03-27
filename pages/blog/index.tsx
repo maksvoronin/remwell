d00a2641cb229d4c64d224495cb2461e610fa59e
Maxim Voronin <catlense@voronin.xyz>
2023-03-28 00:02:25 +0700

    Create blog and feedback pages

---
+++ pages/blog/index.tsx
@@ -0,0 +1,14 @@
+import { Footer, Header } from "@/widgets";
+import Head from "next/head";
+
+export default function Blog() {
+  return(
+    <>
+      <Head>
+        <title>Блог / Ремвелл</title>
+      </Head>
+      <Header />
+      <Footer />
+    </>
+  )
+}
---
+++ pages/feedback.tsx
@@ -0,0 +1,14 @@
+import { Footer, Header } from "@/widgets";
+import Head from "next/head";
+
+export default function Feedback() {
+  return (
+    <>
+      <Head>
+        <title>Отзывы / Ремвелл</title>
+      </Head>
+      <Header />
+      <Footer />
+    </>
+  );
+}
