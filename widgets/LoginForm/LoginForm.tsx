c4c8344582f3e172f72ca6743f4088b426e36d58
Maxim Voronin <catlense@voronin.xyz>
2023-05-15 21:19:36 +0700

    Login form

--- widgets/LoginForm/LoginForm.tsx
+++ widgets/LoginForm/LoginForm.tsx
@@ -1,13 +1,38 @@
 import { Container } from "@/components";
-import { FC } from "react";
+import { FC, useState } from "react";
 import s from "./loginform.module.scss";
+import { useRouter } from "next/router";
 
 const LoginForm: FC = () => {
+  const router = useRouter();
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const sendData = () => {
+    fetch(`/api/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({login, password}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(r => r.json())
+    .then(response => {
+      if(response.code) {
+        if(response.code === 1) return alert("Укажите логин и пароль");
+        if(response.code === 3) return alert("Админ не найден, проверьте логин");
+        if(response.code === 4) return alert("Неправильный пароль");
+        return alert(response.text);
+      }
+      window.localStorage.token = response.token;
+      router.push("/admin/dashboard");
+    });
+  }
+
   return <Container className={s.loginForm}>
     <h1>Вход</h1>
-    <input placeholder="Логин" type="text" />
-    <input placeholder="Пароль" type="password" />
-    <button>Test</button>
+    <input placeholder="Логин" type="text" onChange={({target}) => setLogin(target.value)} value={login} />
+    <input placeholder="Пароль" type="password" onChange={({target}) => setPassword(target.value)} value={password} />
+    <button onClick={sendData}>Войти</button>
   </Container>
 }
 
